2d40336323040cc6899371d682da18bdec926565
zppro <zpprozppro@gmail.com>
2019-04-28 21:59:43 +0800

    check支持

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type AppVer struct {
+	AppId string `json:"appId""`
+	Ver   string `json:"ver""`
+	Url   string `json:"url"`
+	Hash  string `json:"hash"`
+}
+
+type filterFunc func(AppVer) bool
+
+type MKTVersions []AppVer
+
+var mktVers MKTVersions
+
+func (vers MKTVersions) filter(predict filterFunc) []AppVer {
+	filtered := make([]AppVer, len(vers))
+	var last int
+	for i, v := range vers {
+		if match := predict(v); match {
+			filtered[i] = v
+			last++
+		}
+	}
+	return filtered[0:last]
+}
+
+func handlerDefault(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello => %s", r.URL.Path)
+}
+
+func handlerCheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(405)
+		fmt.Fprint(w, "need GET")
+		return
+	}
+	appId := r.URL.Query().Get("appId")
+	if appId == "" {
+		appId = "mkt"
+	}
+
+	ver := r.URL.Query().Get("ver")
+	if ver == "" {
+		ver = "latest"
+	}
+
+
+	/// todo 考虑增加 mkt+ver的filter结果缓存并使其并发安全
+
+	filtered := mktVers.filter(func(item AppVer) bool {
+		return item.Ver == ver
+	})
+
+	data, err := json.Marshal(filtered)
+	if err != nil {
+		log.Fatalf("JSON marshaling faild: %s", err)
+	}
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.Write(data)
+	//fmt.Fprint(w, data)
+}
+
+func isFileExist(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		fmt.Println(info)
+		return false
+	}
+	return true
+}
+
+func readAppVersFromFile(appId string) {
+	filename := fmt.Sprintf("./%s-ver.json", appId)
+	if !isFileExist(filename) {
+		log.Fatalf("文件%s 不存在", filename)
+		return
+	}
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("读取文件%s faild: %s", filename, err)
+		return
+	}
+	//读取的数据为json格式，需要进行解码
+	err = json.Unmarshal(data, &mktVers)
+	if err != nil {
+		log.Fatalf("JSON Unmarshal faild: %s", err)
+		return
+	}
+}
+
+func main() {
+	readAppVersFromFile("mkt")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlerDefault)
+	mux.HandleFunc("/check", handlerCheck)
+	http.ListenAndServe(":8081", mux)
+}
